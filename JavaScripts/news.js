c07bc71964c315d5ba5288968c217baeaab819ba
Mikhail-Litvinov <69105262+Mikhail-Litvinov@users.noreply.github.com>
2020-12-22 11:20:56 +0300

    news menu fix, main background fix

--- JavaScripts/news.js
+++ JavaScripts/news.js
@@ -1,20 +1,20 @@
-$(window).ready(function () {
-    if (/Android|webOS|iPhone|iPad|iPod|BlackBerry|BB|PlayBook|IEMobile|Windows Phone|Kindle|Silk|Opera Mini/i.test(navigator.userAgent)) {
-        $(".right-column").appendTo(".modile-blog-menu")
-    } else {
-        $(".right-column").appendTo(".blog-content")
-    }
-}).resize(function () {
-    if (/Android|webOS|iPhone|iPad|iPod|BlackBerry|BB|PlayBook|IEMobile|Windows Phone|Kindle|Silk|Opera Mini/i.test(navigator.userAgent)) {
-        $(".right-column").appendTo(".modile-blog-menu")
-    } else {
-        $(".right-column").appendTo(".blog-content")
-    }
-})
+// $(window).ready(function () {
+//     if (/Android|webOS|iPhone|iPad|iPod|BlackBerry|BB|PlayBook|IEMobile|Windows Phone|Kindle|Silk|Opera Mini/i.test(navigator.userAgent)) {
+//         $(".right-column").appendTo(".modile-blog-menu")
+//     } else {
+//         $(".right-column").appendTo(".blog-content")
+//     }
+// }).resize(function () {
+//     if (/Android|webOS|iPhone|iPad|iPod|BlackBerry|BB|PlayBook|IEMobile|Windows Phone|Kindle|Silk|Opera Mini/i.test(navigator.userAgent)) {
+//         $(".right-column").appendTo(".modile-blog-menu")
+//     } else {
+//         $(".right-column").appendTo(".blog-content")
+//     }
+// })
 
 $(".mobile-nav-button").click(function () {
-    $(".right-column").toggleClass("oppened");
-        if ($(".right-column").has("oppened")) {
+    $(".mobile-nav-button").toggleClass("oppened");
+        if ($(".mobile-nav-button").has("oppened")) {
         $(".blog-menu-wrapper").height($(".blog-menu-wrapper").get(0).scrollHeight + "px")
         } else {
             $(".blog-menu-wrapper").height(0)
